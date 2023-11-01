d902e3099aa876d74dc6c43b19e6311ac7a0e027
50461 <lime@sk.com>
2023-11-01 19:46:33 +0900

    :sparkles: puppeter -> cheerio

--- src/lib/getMetaTags.ts
+++ src/lib/getMetaTags.ts
@@ -1,43 +1,10 @@
-import puppeteer from 'puppeteer'
 import { load } from 'cheerio'
+import axios from 'axios'
 
-export async function getMetaTags(url: string) {
+export async function getMetaTags(url: string): Promise<{ [key: string]: string } | null> {
   try {
-    const browser = await puppeteer.launch({
-      headless: 'new',
-      args: [
-        '--disable-extensions',
-        '--disable-plugins',
-        '--disable-dev-shm-usage', // Docker와 같은 컨테이너 환경에서 메모리 이슈를 방지
-        '--no-sandbox', // 보안이 중요하지 않은 환경에서 성능 향상을 위해 사용
-        '--disable-gpu', // GPU 가속을 비활성화
-      ],
-    })
-
-    const page = await browser.newPage()
-    await page.goto(url, { waitUntil: 'domcontentloaded' })
-
-    await page.setRequestInterception(true)
-
-    page.on('request', (req) => {
-      if (req.resourceType() === 'stylesheet' || req.resourceType() === 'image') {
-        req.abort()
-      } else {
-        req.continue()
-      }
-    })
-
-    await page.waitForFunction(
-      () => {
-        return document.querySelector('meta[property="og:title"]')
-      },
-      { timeout: 5000 }
-    ) // 5초 타임아웃 설정
-
-    const content = await page.content()
-
-    const $ = load(content)
-
+    const response = await axios.get(url)
+    const $ = load(response.data)
     const metaTags: { [key: string]: string } = {}
 
     $('meta').each((_, element) => {
@@ -48,10 +15,9 @@ export async function getMetaTags(url: string) {
       }
     })
 
-    await browser.close()
-
     return metaTags
   } catch (err) {
+    console.error(err)
     return null
   }
 }
