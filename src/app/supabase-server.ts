8b59a8a9d141467ef6f3aaa9927183167afd5de7
50461 <lime@sk.com>
2023-11-05 11:50:42 +0900

    :sparkles: 이미지 정보가 없는 데이터는 metatag 읽어오기

--- src/app/supabase-server.ts
+++ src/app/supabase-server.ts
@@ -1,7 +1,7 @@
 import { cookies } from 'next/headers'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { Database } from '../types/supabase'
-import { NextResponse } from 'next/server'
+import { getMetaTags } from '@/lib/getMetaTags'
 
 export const createServerSupabaseClient = () =>
   createServerComponentClient<Database>(
@@ -42,7 +42,32 @@ export async function getRegistedUrls(limit = 5) {
       throw new Error('No data found')
     }
 
-    return ogs
+    const promises = ogs.data.map(async (value) => {
+      // 이미 이미지가 있으면, 새로운 값을 찾지 않고 현재 값을 반환합니다.
+      if (value.image) {
+        return value
+      }
+
+      try {
+        // 이미지가 없으면, getMetaTags를 사용하여 이미지를 가져옵니다.
+        const metaTags = await getMetaTags(value.origin_url ?? '')
+        return {
+          ...value,
+          image: metaTags?.['image'], // 안전하게 접근하기 위해 옵셔널 체이닝 사용
+        }
+      } catch (error) {
+        console.error('Error fetching meta tags for URL:', value.origin_url, error)
+        return {
+          ...value,
+          image: null, // 에러 발생 시, 이미지를 null로 설정합니다.
+        }
+      }
+    })
+
+    // Promise.all을 사용하여 모든 프로미스가 해결되기를 기다립니다.
+    const ogsData = await Promise.all(promises)
+
+    return { ...ogs, data: ogsData }
   } catch (err) {
     return null
   }
